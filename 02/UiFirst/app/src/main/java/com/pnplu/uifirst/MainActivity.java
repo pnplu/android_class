7b27a35e42268136b433f4ad28da13affba7825c
Pasit <enqueteplu@gmail.com>
2016-08-29 16:02:51 +0700

    In class 02 calculate

---
+++ 02/UiFirst/app/src/main/java/com/pnplu/uifirst/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pnplu.uifirst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cal);
+    }
+}
